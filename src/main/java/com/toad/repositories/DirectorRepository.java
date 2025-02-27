ae458882fd0c2ccfefd13cbc4f50f34a114a0e3c
Myriam <jb.myriam@gmail.com>
2025-02-27 10:38:31 +0100

    Modification RentalController/Add Integer.Valueof

--- src/main/java/com/toad/repositories/DirectorRepository.java
+++ src/main/java/com/toad/repositories/DirectorRepository.java
@@ -1,11 +1,18 @@
 package com.toad.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.toad.entities.Director;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-public interface DirectorRepository extends CrudRepository<Director, Integer> {
-
+public interface DirectorRepository extends JpaRepository<Director, Integer> {
+    @Query(value = "SELECT d.director_id, d.nom, d.prenom, COUNT(f.film_id) AS nombre_films " +
+                   "FROM director d " +
+                   "LEFT JOIN film_director f ON d.director_id = f.director_id " +
+                   "WHERE d.nom = :nom AND d.prenom = :prenom " +
+                   "GROUP BY d.director_id, d.nom", nativeQuery = true)
+    List<Object[]> findDirectorWithFilmCount(@Param("nom") String nom, @Param("prenom") String prenom);
 }
